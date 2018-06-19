490f1cc99a5a9a0ee5e3bffefb7182adcf32b98a
amcguirebooster <andrewmcguire@boosterthon.com>
2018-06-18 21:38:44 -0400

    Fixed maint. script

---
+++ scripts/maint_mode.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+if [ "$1" == "" ]
+then
+  echo "1 - to enable maintance mode"
+  echo "0 - to disable maintance mode"
+fi
+if [ "$1" == "1" ]
+then
+  mv -v /var/www/html/hawkitsolutions/public/index.html /var/www/html/hawkitsolutions/index.html.disabled
+  cp -v /var/www/html/hawkitsolutions/coming-soon.html /var/www/html/hawkitsolutions/index.html
+fi
+if [ "$1" == "2" ]
+then
+  mv -v /var/www/html/hawkitsolutions/index.html.disabled /var/www/html/hawkitsolutions/index.html
+fi
