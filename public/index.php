15987bf71dba0aa85206dc2057455ad029ec2e58
amcguirebooster <andrewmcguire@boosterthon.com>
2018-06-21 12:16:06 -0400

    Fixed mx.php

--- public/index.php
+++ public/index.php
@@ -13,7 +13,7 @@ include("layout/c794bc6c9a3eccc2.php");
   </div>
 <!-- end preloader -->
 <!-- navigation -->
-  <header id="nav-bar" class="header-main-block nav-bar fixed-top page-nav">
+  <header id="nav-bar" class="header-main-block nav-bar fixed-top">
     <div class="container">
       <div class="row">
         <div class="col-xs-3 pad-0">
@@ -21,35 +21,34 @@ include("layout/c794bc6c9a3eccc2.php");
             <a href="index.html" title="Logo"><img src="images/logo.png" class="img-responsive" alt="logo"></a>
           </div>
         </div>
+        <div class="col-xs-9 text-right">
+          <div class="menu-btn">
+            <a href="#" id="menu-btn" title="Menu"><i class="fa fa-bars"></i></a>
+          </div>
+        </div>
+      </div>
       <nav id="site-nav" class="nav-overlay">
-        <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+        <a href="javascript:void(0)" id="close-btn" class="closebtn">&times;</a>
         <ul class="nav-overlay-content">
           <li class="nav-label">Menu</li>
-          <li class="active"><a href="index.html" title="Home">Home</a></li>
-          <li><a href="404.html" title="About">About</a></li>
-          <li><a href="404.html title="Services">Services</a></li>
-          <!--<li><a href="404.html" title="Documents">Documents</a></li>-->
+          <li class="active"><a href="index.php" title="Home">Home</a></li>
+          <li><a href="#" title="About">About</a></li>
+          <li><a href="#" title="Services">Services</a></li>
           <li class="nav-label">Utilities</li>
-          <li><a href="mx.php" title="XMR">XMR Miner</a></li>
-          <li class="nav-label">User Dashboard</li>
-          <li><a href="login.php" title="Login">Login</a></li>
+          <li><a href="mx.php" title="XMRMiner">XMR Miner</a></li>
+          <li><a href="#" title="Downloads">Downloads</a></li>
+          <li><a href="#" title="MemberArea">Member Area</a></li>
           <li class="nav-label">Contact</li>
           <li><a href="contact.html" title="Contact">Contact</a></li>
-          <div class="search-container">
-            <form action="action_page.php">
-              <input type="text" placeholder="Search.." name="search">
-              <button type="submit"><i class="fa fa-search"></i></button>
-            </form>
-          </div>
         </ul>
         <div class="nav-social text-center">
           <div class="nav-label">Follow Us</div>
           <ul>
-            <li><a href="https://www.facebook.com/" title="Facebook" target="_blank"><i class="fa fa-facebook"></i></a></li>
-            <li><a href="https://www.twitter.com/" title="Twitter" target="_blank"><i class="fa fa-twitter"></i></a></li>
-            <li><a href="https://www.instagram.com/" title="Instagram" target="_blank"><i class="fa fa-instagram"></i></a></li>
-            <li><a href="https://www.dribbble.com/" title="Dribbble" target="_blank"><i class="fa fa-dribbble"></i></a></li>
-            <li><a href="https://www.linkedin.com/" title="Linkedin" target="_blank"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="https://www.facebook.com/" target="_blank" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="https://www.twitter.com/" target="_blank" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+            <li><a href="https://www.instagram.com/" target="_blank" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+            <li><a href="https://www.dribbble.com/" target="_blank" title="Dribbble"><i class="fa fa-dribbble"></i></a></li>
+            <li><a href="https://www.linkedin.com/" target="_blank" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
           </ul>
         </div>
       </nav>
